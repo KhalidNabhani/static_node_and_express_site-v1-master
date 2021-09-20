9ade05c2ffbed429ccebb57618dafd37ea9050eb
KhalidNabhani <khalid.suleiman.hemyer@gmail.com>
2021-09-20 20:34:38 +0400

    add not-found page tempalte

--- app.js
+++ app.js
@@ -9,8 +9,6 @@ const app = express();
 
 app.use(express.json());
 app.use('/static', express.static('public'));
-app.use('/views', express.static('views'));
-app.use('/images', express.static('images'));
 
 app.set('view engine', 'pug');
 
@@ -20,25 +18,65 @@ app.set('view engine', 'pug');
 /*
   root page route 
 */
-app.get('/',(req, res)=>{
-    res.locals.projects = projects;
-    console.log(res.locals.projects);
-    res.render('index');
+app.get('/', function (req, res){
+    
+    res.render('index',{projects});
 })
 
 /*
   project  page route
   @param prject id  
 */
-app.get((req, res, next) =>{
-    res.render('/prjects:id');
+app.get("/project/:id", function (req, res, next) {
+    const projId = req.params.id;
+    const proj= projects.find( ({ id }) => id === +projId );
+    res.render('project',{proj});
     next();
 })
-    
-/*
-app.set('views', path.join(__dirname, 'views'));
 
+/*
+  About  page route
+  @param prject id  
 */
+app.get("/about", function (req, res, next) {
+    res.render('about');
+    next();
+})
+
+/*
+    error handlers 
+*/
+app.use((req, res, next) => {
+
+    console.log('404 error handler called');
+    res.status(404).render('not-found');
+  });
+
+/* Global error handler */
+app.use((err, req, res, next) => {
+
+    if (err) {
+      console.log('Global error handler called', err);
+    }
+  
+    /* TODO 2: Handle errors caught by your route handlers
+      - If the error status is 404:
+          * Set the response status to 404
+          * Render the 'not-found' view and pass the error object to the view
+      - Else:
+          * Set the error message to the given message, or specify a general, 
+            default error message
+          * Set response status to the given error status OR, set it to 500 by default if no error status is set
+          * Render the 'error' view, passing it the error object
+    */
+    if (err.status === 404) {
+      res.status(404).render('not-found', { err });
+    } else {
+      err.message = err.message || `Oops!  It looks like something went wrong on the server.`;
+      res.status(err.status || 500).render('error', { err });
+    }
+});
+
 
 
 app.listen(port, () => {
