72f75326f5888af5d3ad3101828041730221c54b
KhalidNabhani <khalid.suleiman.hemyer@gmail.com>
2021-09-21 20:56:38 +0400

    fixed error handlers 404 and 500

--- app.js
+++ app.js
@@ -7,9 +7,7 @@ const {projects} = require('./data.json');
 const port =7000;
 const app = express();
 
-app.use(express.json());
 app.use('/static', express.static('public'));
-
 app.set('view engine', 'pug');
 
 /*
@@ -31,7 +29,7 @@ app.get("/project/:id", function (req, res, next) {
     const projId = req.params.id;
     const proj= projects.find( ({ id }) => id === +projId );
     res.render('project',{proj});
-    next();
+    
 })
 
 /*
@@ -40,7 +38,7 @@ app.get("/project/:id", function (req, res, next) {
 */
 app.get("/about", function (req, res, next) {
     res.render('about');
-    next();
+   
 })
 
 /*
@@ -48,34 +46,21 @@ app.get("/about", function (req, res, next) {
 */
 app.use((req, res, next) => {
 
-    console.log('404 error handler called');
-    res.status(404).render('not-found');
+
+  console.log('404 error handler called');
+  res.status(404).render('not-found');
+
+  
   });
 
 /* Global error handler */
 app.use((err, req, res, next) => {
-
-    if (err) {
-      console.log('Global error handler called', err);
-    }
-  
-    /* TODO 2: Handle errors caught by your route handlers
-      - If the error status is 404:
-          * Set the response status to 404
-          * Render the 'not-found' view and pass the error object to the view
-      - Else:
-          * Set the error message to the given message, or specify a general, 
-            default error message
-          * Set response status to the given error status OR, set it to 500 by default if no error status is set
-          * Render the 'error' view, passing it the error object
-    */
-    if (err.status === 404) {
-      res.status(404).render('not-found', { err });
-    } else {
-      err.message = `Oops!  It looks like something went wrong on the server.`;
-      res.status(err.status= 500).render('error', { err });
-    }
-  });
+  err.status= 500;
+  err.message = `Oops!  It looks like something went wrong on the server.`;
+  console.log('500 Global error handler called');
+  res.status(err.status).render('error', { err });
+    
+});
 
 
 
